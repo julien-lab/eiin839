472699b0af6d1628ce5d0ec9b4586bd3524d8e1f
julien-lab <julienn.ndiaye59155@gmail.com>
2021-04-14 17:03:12 +0200

    ADD TD4

---
+++ TD4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD4.Calculator;
+
+namespace TD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CalculatorSoapClient calc = new CalculatorSoapClient(new CalculatorSoapClient.EndpointConfiguration(), "http://www.dneonline.com/calculator.asmx?wsdl");
+            CalculatorSoapClient calc = new CalculatorSoapClient();
+            Console.WriteLine(calc.Add(12, 4));
+            while (true)
+            {
+
+            }
+        }
+    }
+}
